f82f309b6720d50228f61f283d2797977ff4e4ee
Enoki <enokiun@gmail.com>
2022-11-10 13:53:42 +0300

    refactor: improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{
     collections::HashMap,
-    env,
+    env, error,
     io::{BufRead, BufReader, Read, Write},
-    net::{SocketAddr, TcpListener},
+    net::{SocketAddr, TcpListener, TcpStream},
     thread,
 };
 
@@ -12,6 +12,7 @@ struct Request {
     peer_addr: Option<SocketAddr>,
     method: String,
     route: String,
+    http: String,
     headers: HashMap<String, String>,
     body: Option<String>,
 }
@@ -21,51 +22,71 @@ fn main() {
     let listener = TcpListener::bind(&address)
         .unwrap_or_else(|_| panic!("Could not start TCP server at {}", address));
     for stream in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = stream.unwrap();
-            let mut buf = BufReader::new(&mut stream);
-            let mut first = String::new();
-            let mut content_length = 0;
-            let mut body: Option<String> = None;
-            let mut headers: HashMap<String, String> = HashMap::new();
-            let mut headers_done = false;
-            buf.read_line(&mut first).unwrap();
-            let mut first = first.split(' ');
-            loop {
-                if headers_done {
-                    if content_length == 0 {
-                        break;
-                    }
-                    let mut body_buf = vec![0; content_length];
-                    buf.read_exact(&mut body_buf).unwrap();
-                    body = Some(String::from_utf8(body_buf.to_vec()).unwrap());
-                    break;
+        thread::spawn(move || match stream {
+            Ok(stream) => {
+                if let Err(err) = handle_request(stream) {
+                    eprintln!("Could not extract data from request: {}", err);
                 }
-                let mut l = String::new();
-                buf.read_line(&mut l).unwrap();
-                l.pop();
-                l.pop();
-                if l.is_empty() {
-                    headers_done = true;
-                    continue;
-                }
-                let (header, value) = l.split_once(": ").unwrap();
-                if header == "Content-Length" {
-                    content_length = value.parse().unwrap();
-                }
-                headers.insert(header.to_string(), value.to_string());
             }
-            let request = Request {
-                peer_addr: stream.peer_addr().ok(),
-                method: first.next().unwrap().to_string(),
-                route: first.next().unwrap().to_string(),
-                headers,
-                body,
-            };
-            println!("{:#?}", request);
-            stream
-                .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
-                .unwrap();
+            Err(err) => {
+                eprintln!("Encountered error with connection: {}", err);
+            }
         });
     }
+}
+
+fn handle_request(mut stream: TcpStream) -> Result<(), Box<dyn error::Error>> {
+    let mut buf = BufReader::new(&mut stream);
+    let mut first = String::new();
+    let mut content_length = 0;
+    let mut body: Option<String> = None;
+    let mut headers: HashMap<String, String> = HashMap::new();
+    let mut headers_done = false;
+    buf.read_line(&mut first)?;
+    let mut first = first.split(' ');
+
+    loop {
+        if headers_done {
+            if content_length == 0 {
+                break;
+            }
+            let mut body_buf = vec![0; content_length];
+            buf.read_exact(&mut body_buf)?;
+            body = Some(String::from_utf8(body_buf.to_vec())?);
+            break;
+        }
+        let mut l = String::new();
+        buf.read_line(&mut l)?;
+        l.pop();
+        l.pop();
+        if l.is_empty() {
+            headers_done = true;
+            continue;
+        }
+        let (header, value) = l.split_once(": ").ok_or("Malformed header syntax")?;
+        if header == "Content-Length" {
+            content_length = value.parse()?;
+        }
+        headers.insert(header.to_string(), value.to_string());
+    }
+
+    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+
+    let method = first.next().ok_or("Malformed HTTP request")?.to_string();
+    let route = first.next().ok_or("Malformed HTTP request")?.to_string();
+    let mut http = first.next().ok_or("Malformed HTTP request")?.to_string();
+    http.pop();
+    http.pop();
+
+    let request = Request {
+        peer_addr: stream.peer_addr().ok(),
+        method,
+        route,
+        http,
+        headers,
+        body,
+    };
+    println!("{:#?}", request);
+
+    Ok(())
 }
